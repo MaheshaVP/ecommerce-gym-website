0cb681d6209f93e07a1cccc8d5397a70c7d38179
Mahesha V P <maheshavp12@gmail.com>
2025-05-10 23:26:07 +0530

    ecommerce

--- src/components/Rout.js
+++ src/components/Rout.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import { Routes , Route } from 'react-router'
-import Home from './Home'
-import Shop from './Shop'
-import Cart from './Cart'
-import About from './About'
-import Contact from './Contact'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Shop from './Shop';
+import Cart from './Cart';
+import About from './About';
+import Contact from './Contact';
 
-const Rout = ({shop , Filter , allcateFilter , addtocart , cart , setCart}) => {
+const Rout = ({ shop, Filter, allcateFilter, addtocart, cart, setCart }) => {
   return (
-    <>
     <Routes>
-        <Route path='/' element={<Home addtocart={addtocart}/>} />
-        <Route path='/cart' element={<Cart cart={cart} setCart={setCart} />} />
-        <Route path='/shop' element={<Shop shop={shop} Filter={Filter} allcateFilter={allcateFilter} addtocart={addtocart} />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
+      <Route path='/' element={<Home addtocart={addtocart} />} />
+      <Route path='/cart' element={<Cart cart={cart} setCart={setCart} />} />
+      <Route path='/shop' element={<Shop shop={shop} Filter={Filter} allcateFilter={allcateFilter} addtocart={addtocart} />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
     </Routes>
-    </>
-  )
-}
+  );
+};
 
-export default Rout
+export default Rout;
