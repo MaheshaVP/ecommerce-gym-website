7563dc7ef3fd9eef16a826657eb0cfca9dfb6c62
Mahesha V P <maheshavp12@gmail.com>
2025-05-26 23:35:56 +0530

    e-commerce gym website

--- src/components/Rout.js
+++ src/components/Rout.js
@@ -5,12 +5,11 @@ import Shop from "./Shop";
 import Cart from "./Cart";
 import About from "./About";
 import Contact from "./Contact";
-import NotFound from "./NotFound"; // Make sure the path is correct
 
 const Rout = ({ shop, Filter, allcateFilter, addtocart, cart, setCart }) => {
   return (
     <Routes>
-      <Route path="/" element={<Home addtocart={addtocart} />} />
+      <Route path="*" element={<Home addtocart={addtocart} />} />
       <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
       <Route
         path="/shop"
@@ -25,7 +24,6 @@ const Rout = ({ shop, Filter, allcateFilter, addtocart, cart, setCart }) => {
       />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} /> {/* This will catch all undefined routes */}
     </Routes>
   );
 };
